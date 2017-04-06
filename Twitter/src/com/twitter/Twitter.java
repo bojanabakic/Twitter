0e1a3476bb3de0d27b7f3c13fb93e9191590a237
bojanabakic <bojanabakic96@gmail.com>
2017-04-06 15:20:44 +0200

    Dokumentovana javna klasa Twitter i generisana javaDoc HTML strana.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -3,13 +3,35 @@ package com.twitter;
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
 
+/**
+ * 
+ * Klasa uz pomoc 2 metode vrsi unos twitova u listu poruke i puni niz objekata tipa TwitterPoruka
+ * porukama sa proslijedjenim tagom u vidu parametra
+ * @author Bojana Bakic
+ * @version 1.0
+ *
+ */
 public class Twitter {
+	/**
+	 * Lista objekata tipa TwitterPoruka naziva poruke
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
-
+	
+	/**
+	 * Metoda koja vraca listu koja sadrzi twitove
+	 * @return listu objekata tipa TwitterPoruka
+	 */
+	
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
 
+	/**
+	 * Metoda koja pravi objekat tipa TwitterPoruka i unosi parametre korisnik i poruka
+	 * koji su proslijedjeni i nakon toga je unosi na kraj liste poruke
+	 * @param korisnik tipa String predstavlja naziv korisnika
+	 * @param poruka tipa String predstavlja tekst poruke
+	 */
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -18,7 +40,20 @@ public class Twitter {
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
 	}
-
+	
+	/**
+	 * Metoda koja vraca niz poruka koje pretrazujemo preko taga kojeg unosimo kao 
+	 * parametar metode
+	 * @param maxBroj tipa int prestavlja maksimalan broj poruka u nizu
+	 * @param tag tipa String predstavlja tag po kojem se poruke pretrazuju
+	 * @return niz objekata tipa TwitterPoruka duzine maxBroj koji sadrzi poruke sa pretrazivanim tagom
+	 * @throws java.lang.RuntimeException ako je unijeti tag:
+	 * 
+	 * <ul>
+	 * <li>null</li>
+	 * <li>prazan String</li>
+	 * </ul>
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag.isEmpty())
 			throw new RuntimeException("Morate uneti tag");
